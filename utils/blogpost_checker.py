2b1872ab797e4898ee90a9f8b69d6047b5f7fd8a
David Feinblum <dvfeinblum@gmail.com>
2021-07-12 01:29:47 -0400

    final sprucings

--- utils/blogpost_checker.py
+++ utils/blogpost_checker.py
@@ -15,6 +15,9 @@ def get_post(blog_table: boto3.resource, post_id: str):
     Attempts to fetch post from blog_post table.
     """
     response = blog_table.get_item(Key={"post_id": post_id})
+    if not response:
+        print(f"Got a weird post object from blogger:\n{blog_table}")
+        exit(1)
     return response.get("Item")
 
 
@@ -41,15 +44,14 @@ def parse_feed(cfg: Config):
         if published_at > latest_published_at:
             latest_post = post
             latest_published_at = published_at
-
-    dynamodb = boto3.resource("dynamodb")
-    blog_table = dynamodb.Table("avobot_blog_posts")
     if not latest_post:
         # This really shouldn't happen but for completeness
         print("No latest post. Maybe the RSS didn't load correctly?")
 
+    dynamodb = boto3.resource("dynamodb")
+    blog_table = dynamodb.Table("avobot_blog_posts")
     latest_id = latest_post.select("id")[0].text
-    if not get_post(blog_table, latest_id) or True:
+    if not get_post(blog_table, latest_id):
         print("New blogpost detected; updating table.")
         post_title = latest_post.select("title")[0].text
         blog_table.put_item(
@@ -64,7 +66,6 @@ def parse_feed(cfg: Config):
             "@dvfeinblum just published a new blogpost! Check it out at https://avogadbro.blogspot.com/",
             cfg,
         )
-
     else:
         print("No new posts detected.")
 
